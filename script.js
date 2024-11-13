ce5fa9351c5147b3239cf1df75a5559e0ba207c3
Taraka2027 <mhtaraka2027@gmail.com>
2024-11-13 08:33:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,133 @@
+document.getElementById('message2').style.display = 'none';
+document.getElementById('message3').style.display = 'none';
+document.getElementById('message4').style.display = 'none';
+document.getElementById('message5').style.display = 'none';
+document.getElementById('message6').style.display = 'none';
+document.getElementById('message7').style.display = 'none';
+document.getElementById('message8').style.display = 'none';
+document.getElementById('message9').style.display = 'none';
+document.getElementById('message10').style.display = 'none';
+document.getElementById('message11').style.display = 'none';
+document.getElementById('message12').style.display = 'none';
+document.getElementById('message13').style.display = 'none';
+document.getElementById('message14').style.display = 'none';
+document.getElementById('message15').style.display = 'none';
+document.getElementById('message16').style.display = 'none';
+document.getElementById('message17').style.display = 'none';
+document.getElementById('message18').style.display = 'none';
+document.getElementById('message19').style.display = 'none';
+document.getElementById('message20').style.display = 'none';
+document.getElementById('message21').style.display = 'none';
+document.getElementById('message22').style.display = 'none';
+document.getElementById('message23').style.display = 'none';
+document.getElementById('message24').style.display = 'none';
+document.getElementById('message25').style.display = 'none';
+document.getElementById('message26').style.display = 'none';
+document.getElementById('message27').style.display = 'none';
+
+document.getElementById('next1').onclick = function() {
+    document.getElementById('message1').style.display = 'none';
+    document.getElementById('message2').style.display = 'block';
+}
+document.getElementById('next2').onclick = function() {
+    document.getElementById('message2').style.display = 'none';
+    document.getElementById('message3').style.display = 'block';
+}
+document.getElementById('next3').onclick = function() {
+    document.getElementById('message3').style.display = 'none';
+    document.getElementById('message4').style.display = 'block';
+}
+document.getElementById('next4').onclick = function() {
+    document.getElementById('message4').style.display = 'none';
+    document.getElementById('message5').style.display = 'block';
+}
+document.getElementById('next5').onclick = function() {
+    document.getElementById('message5').style.display = 'none';
+    document.getElementById('message6').style.display = 'block';
+}
+document.getElementById('next6').onclick = function() {
+    document.getElementById('message6').style.display = 'none';
+    document.getElementById('message7').style.display = 'block';
+}
+document.getElementById('next7').onclick = function() {
+    document.getElementById('message7').style.display = 'none';
+    document.getElementById('message8').style.display = 'block';
+}
+document.getElementById('next8').onclick = function() {
+    document.getElementById('message8').style.display = 'none';
+    document.getElementById('message9').style.display = 'block';
+}
+document.getElementById('next9').onclick = function() {
+    document.getElementById('message9').style.display = 'none';
+    document.getElementById('message10').style.display = 'block';
+}
+document.getElementById('next10').onclick = function() {
+    document.getElementById('message10').style.display = 'none';
+    document.getElementById('message11').style.display = 'block';
+}
+document.getElementById('next11').onclick = function() {
+    document.getElementById('message11').style.display = 'none';
+    document.getElementById('message12').style.display = 'block';
+}
+document.getElementById('next12').onclick = function() {
+    document.getElementById('message12').style.display = 'none';
+    document.getElementById('message13').style.display = 'block';
+}
+document.getElementById('next13').onclick = function() {
+    document.getElementById('message13').style.display = 'none';
+    document.getElementById('message14').style.display = 'block';
+}
+document.getElementById('next14').onclick = function() {
+    document.getElementById('message14').style.display = 'none';
+    document.getElementById('message15').style.display = 'block';
+}
+document.getElementById('next15').onclick = function() {
+    document.getElementById('message15').style.display = 'none';
+    document.getElementById('message16').style.display = 'block';
+}
+document.getElementById('next16').onclick = function() {
+    document.getElementById('message16').style.display = 'none';
+    document.getElementById('message17').style.display = 'block';
+}
+document.getElementById('next17').onclick = function() {
+    document.getElementById('message17').style.display = 'none';
+    document.getElementById('message18').style.display = 'block';
+}
+document.getElementById('next18').onclick = function() {
+    document.getElementById('message18').style.display = 'none';
+    document.getElementById('message19').style.display = 'block';
+}
+document.getElementById('next19').onclick = function() {
+    document.getElementById('message19').style.display = 'none';
+    document.getElementById('message20').style.display = 'block';
+}
+
+document.getElementById('next20').onclick = function() {
+    document.getElementById('message20').style.display = 'none';
+    document.getElementById('message21').style.display = 'block';
+}
+document.getElementById('next21').onclick = function() {
+    document.getElementById('message21').style.display = 'none';
+    document.getElementById('message22').style.display = 'block';
+}
+document.getElementById('next22').onclick = function() {
+    document.getElementById('message22').style.display = 'none';
+    document.getElementById('message23').style.display = 'block';
+}
+document.getElementById('next23').onclick = function() {
+    document.getElementById('message23').style.display = 'none';
+    document.getElementById('message24').style.display = 'block';
+}
+document.getElementById('next24').onclick = function() {
+    document.getElementById('message24').style.display = 'none';
+    document.getElementById('message25').style.display = 'block';
+}
+document.getElementById('next25').onclick = function() {
+    document.getElementById('message25').style.display = 'none';
+    document.getElementById('message26').style.display = 'block';
+}
+document.getElementById('next26').onclick = function() {
+    document.getElementById('message26').style.display = 'none';
+    document.getElementById('message27').style.display = 'block';
+}
+
